ccb9ae8ee0130e8af2181c33bdfe86ec57aed6e3
Jeremy Gieske <jeremy.gieske@gmail.com>
2021-12-11 14:20:40 -0600

    added random number

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,22 +3,32 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40, 60);
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   if (promptFight === "fight" || promptFight === "FIGHT"){
     while (enemyHealth > 0){
-      enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
         if (enemyHealth <= 0) { 
             window.alert(enemyName + " has died!");
           } else 
             {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
             }
@@ -34,7 +44,7 @@ var fight = function(enemyName) {
     var confirmSkip = window.confirm("Are you sure you'd like to leave the fight? ")
         if (confirmSkip) {
           window.alert(playerName + " has chosen to skip the fight! Goodbye!");
-          playerMoney = playerMoney - 2;
+          Math.max(0, playerMoney = playerMoney - 2);
           } else
             {
               fight();
@@ -57,7 +67,7 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       fight(pickedEnemyName);
 
